aa2610bc31e8db5f2dd7b3c440ea732ed8c9dfe4
Selorme <ruthacolatse.official@gmail.com>
2024-09-11 01:25:02 +0300

    updated entire file

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    year = datetime.now().year
+    return render_template('index.html', copyright_year=year)
+
+if __name__ == '__main__':
+    app.run(debug=True)
