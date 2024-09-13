5f0e9bba40dc5ceef6e88767271ac764b65dd98f
Selorme <ruthacolatse.official@gmail.com>
2024-09-13 18:32:27 +0300

    updated everything

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, url_for, request
 import smtplib
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route("/")
@@ -9,42 +12,42 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/about.html")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
 
-@app.route("/announcements.html")
+@app.route("/announcements")
 def announcements():
     return render_template("announcements.html")
 
 
-@app.route("/president.html")
+@app.route("/president")
 def president():
     return render_template("president.html")
 
 
-@app.route("/secretary.html")
+@app.route("/secretary")
 def secretary():
     return render_template("secretary.html")
 
 
-@app.route("/organizer.html")
+@app.route("/organizer")
 def organizer():
     return render_template("organizer.html")
 
 
-@app.route("/treasurer.html")
+@app.route("/treasurer")
 def treasurer():
     return render_template("treasurer.html")
 
 
-@app.route("/media.html")
+@app.route("/media")
 def media():
     return render_template("media.html")
 
 
-@app.route("/message.html")
+@app.route("/message")
 def message():
     return render_template("message.html")
 
@@ -63,7 +66,8 @@ def contact():
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(user=my_email, password=password)
-            connection.sendmail(from_addr=email, to_addrs=my_email, msg=f"Subject: New Message From Your Website!\n\nName: {name}\nEmail address: {email}\nPhone number: {phone}\nMessage: {messages}")
+            connection.sendmail(from_addr=email, to_addrs=my_email,
+                                msg=f"Subject: New Message From Your Website!\n\nName: {name}\nEmail address: {email}\nPhone number: {phone}\nMessage: {messages}")
         return render_template("contact.html", message_sent=True)
     return render_template("contact.html", message_sent=False)
 
